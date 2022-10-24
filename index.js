ec943a1f2c569641a9a1c395d6d027b04b746389
Risan Ahamed <risanzahamed76@gmail.com>
2022-10-24 21:42:34 +0600

    course data server working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+const coursesData = require("./courseData.json")
 
 const Port = process.env.Port || 5000;
 
@@ -14,6 +15,10 @@ app.get("/", (req, res) => {
     res.send("server is running")
 })
 
+
+app.get("/courses",(req, res)=>{
+  res.send(coursesData)
+})
 
 // app.get("/", (req, res)=>{
 //     res.send("server is working")
